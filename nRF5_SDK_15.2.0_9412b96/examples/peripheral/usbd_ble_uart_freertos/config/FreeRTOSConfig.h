0d80b5ba0238dfd5d0e7c367ca18c5240033cada
leoadvance <leo_advance@163.com>
2019-01-25 10:50:23 +0800

    修改任务名称长度限制

--- nRF5_SDK_15.2.0_9412b96/examples/peripheral/usbd_ble_uart_freertos/config/FreeRTOSConfig.h
+++ nRF5_SDK_15.2.0_9412b96/examples/peripheral/usbd_ble_uart_freertos/config/FreeRTOSConfig.h
@@ -64,7 +64,7 @@
 #define configMAX_PRIORITIES                                                      ( 32 )
 #define configMINIMAL_STACK_SIZE                                                  ( 60 )
 #define configTOTAL_HEAP_SIZE                                                     81920
-#define configMAX_TASK_NAME_LEN                                                   ( 4 )
+#define configMAX_TASK_NAME_LEN                                                   ( 16 )
 #define configUSE_16_BIT_TICKS                                                    0
 #define configIDLE_SHOULD_YIELD                                                   1
 #define configUSE_MUTEXES                                                         1
